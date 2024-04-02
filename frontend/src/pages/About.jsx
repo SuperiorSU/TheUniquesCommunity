7d4fd79e2c62558c59ae87e800a9d9f33ebd3618
Nemmen <Aryankammboz@gmail.com>
2024-04-02 22:30:50 +0530

    new

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/about/Hero'
+import Stories from '../components/about/Stories'
+
+const About = () => {
+  return (
+    <div>
+        
+        <Hero />
+        <Stories />
+    </div>
+  )
+}
+
+export default About
