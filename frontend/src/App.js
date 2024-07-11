5a1efa3fe6d34f390e7fd0c3aee3a4209a850060
aditya raj <adityaraj5555kks@gmail.com>
2024-07-11 12:55:09 +0530

    events updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ function App() {
 
   const event = [
     {
-      id:4,
+      id: 4,
       chapter: "The Uniques | SVIET",
       headerImage: "https://media.licdn.com/dms/image/D5622AQEEj3rY4VRJYA/feedshare-shrink_2048_1536/0/1711181295676?e=1715212800&v=beta&t=biU5tSaDzVZaV1pV-xeNvlSmvUY9ZilyuZ1C0ZLtnT8",
       logo: "https://media.licdn.com/dms/image/D4E22AQHG3lpve4KsoA/feedshare-shrink_1280/0/1710927341125?e=1715212800&v=beta&t=3Xj3TaewuFj1Lgo7luHp8duirsCX7vLgiDmUjg2uQFw",
@@ -177,13 +177,13 @@ function App() {
       partners: [
         "https://sviet.ac.in/wp-content/uploads/2021/08/svietlogooo-1-400x83.png",
         "https://tse3.mm.bing.net/th?id=OIP.T5dL6Z8d1-ElueVdCzdipAHaEK&pid=Api&P=0&h=220",
-        "https://up.yimg.com/ib/th?id=OIP.qhAPWvYnIyuJMR-rdUWrJgHaEK&%3Bpid=Api&rs=1&c=1&qlt=95&w=203&h=114","https://1000logos.net/wp-content/uploads/2020/08/Logo-Infosys-1024x640.jpg","https://tse1.mm.bing.net/th?id=OIP.YPQMM0nCBfk8-Y77Cw5LvAHaHa&pid=Api&P=0&h=220","https://tse2.mm.bing.net/th?id=OIP.b94pLdM9jhCGyKruL9CL8wAAAA&pid=Api&P=0&h=220","https://s3.amazonaws.com/handshake.production/app/public/assets/institutions/179905/cover/hs-emp-branding-image-data.?1501078107", "https://media.glassdoor.com/sqll/353722/ids-infotech-limited-squarelogo-1503985023634.png" ,"https://tse2.mm.bing.net/th?id=OIP.j3mxZxVy_u1U8gWHVGV24AHaBM&pid=Api&P=0&h=220"
+        "https://up.yimg.com/ib/th?id=OIP.qhAPWvYnIyuJMR-rdUWrJgHaEK&%3Bpid=Api&rs=1&c=1&qlt=95&w=203&h=114", "https://1000logos.net/wp-content/uploads/2020/08/Logo-Infosys-1024x640.jpg", "https://tse1.mm.bing.net/th?id=OIP.YPQMM0nCBfk8-Y77Cw5LvAHaHa&pid=Api&P=0&h=220", "https://tse2.mm.bing.net/th?id=OIP.b94pLdM9jhCGyKruL9CL8wAAAA&pid=Api&P=0&h=220", "https://s3.amazonaws.com/handshake.production/app/public/assets/institutions/179905/cover/hs-emp-branding-image-data.?1501078107", "https://media.glassdoor.com/sqll/353722/ids-infotech-limited-squarelogo-1503985023634.png", "https://tse2.mm.bing.net/th?id=OIP.j3mxZxVy_u1U8gWHVGV24AHaBM&pid=Api&P=0&h=220"
 
-        
+
       ]
     },
     {
-      id:5,
+      id: 5,
       chapter: "The Uniques | SVIET",
       headerImage: "https://media.licdn.com/dms/image/D4D22AQEL6ULQQjmG6A/feedshare-shrink_2048_1536/0/1698642601892?e=1715212800&v=beta&t=_2O83ldmUbezl9BKWB342FaqhWalhWZPeTA_9BBRyN0",
       logo: "https://google-ideate-ideathon.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2Fe42010dbd6fd4e8ba4686665f50e7a64%2Fassets%2Ffavicon%2F76.jpeg&w=768&q=75",
@@ -192,60 +192,60 @@ function App() {
       venue: "SVIET Campus",
       overview: "Gear up for the Google Ideate Ideathon 2024! Join us in showcasing innovative ideas that push the boundaries of technology. Collaborate with like-minded individuals, present your groundbreaking concepts, and vie for recognition. Unleash your creativity and be part of shaping the future. Don't miss this chance to make your mark on the world of innovation!",
       speakers: [
-          {
-              id: 1,
-              image: "https://media.licdn.com/dms/image/D5603AQEVP9NUGo4_6g/profile-displayphoto-shrink_200_200/0/1708107118265?e=1718236800&v=beta&t=eYMDWLyYegqsi3zo1fw0S4CytI57G5N-_HFIGCBNg8E",
-              name: "Gaurav Mahipal",
-              company: "Veritos InfoSolutions Pvt Ltd",
-              designation: "Managing Director",
-              profile: "",
-              twitter: "https://twitter.com/cammykamal",
-              linkedin: "https://www.linkedin.com/in/kamal-vaid/",
-              bio: "Kamal loves to work and explore Android. Working as Technology Lead with 8+ years of experience, he believes in the theory of sharing whatever you have learnt from everywhere. He is a blogger and write whenever he feel he has learnt something new and that needs to be shared. He is fond of Cars and Bikes, and spend his free time in riding."
-          },
-          {
-              id: 2,
-              image: "https://media.licdn.com/dms/image/C5603AQHf7VJG-0wVug/profile-displayphoto-shrink_800_800/0/1644000811499?e=1718236800&v=beta&t=Q5foIfAxuxeIMLbyGskDzoV1i6Ic3BMGBJeIwgZkNwQ",
-              name: "Harish Kumar",
-              company: "Paras Technologies",
-              designation: "Founder & CEO",
-              profile: "",
-              twitter: "https://twitter.com/hifiveyatin",
-              linkedin: "https://www.linkedin.com/in/hifiveyatin/",
-              bio: "Yatin is a Project Engineer by profession having 3+ years of experience in corporate field. Learning new technology and implementing them is his goal. Along with his daily life, he also try to give back to the society by sharing information to peers."
-          },
-          {
-              id: 3,
-              image: "https://media.licdn.com/dms/image/D5603AQGV5uzbY5cT_Q/profile-displayphoto-shrink_800_800/0/1686818441933?e=1718236800&v=beta&t=1BCENIx2Jf08IuwIqE8D_a2dJs3wBulvvO-N1UvyGLg",
-              name: "Arun Kumar",
-              company: "Tech Exponent System",
-              designation: "Director",
-              profile: "",
-              twitter: "https://twitter.com/loveleen_nancy",
-              linkedin: "https://www.linkedin.com/in/loveleen-kaur/",
+        {
+          id: 1,
+          image: "https://media.licdn.com/dms/image/D5603AQEVP9NUGo4_6g/profile-displayphoto-shrink_200_200/0/1708107118265?e=1718236800&v=beta&t=eYMDWLyYegqsi3zo1fw0S4CytI57G5N-_HFIGCBNg8E",
+          name: "Gaurav Mahipal",
+          company: "Veritos InfoSolutions Pvt Ltd",
+          designation: "Managing Director",
+          profile: "",
+          twitter: "https://twitter.com/cammykamal",
+          linkedin: "https://www.linkedin.com/in/kamal-vaid/",
+          bio: "Kamal loves to work and explore Android. Working as Technology Lead with 8+ years of experience, he believes in the theory of sharing whatever you have learnt from everywhere. He is a blogger and write whenever he feel he has learnt something new and that needs to be shared. He is fond of Cars and Bikes, and spend his free time in riding."
+        },
+        {
+          id: 2,
+          image: "https://media.licdn.com/dms/image/C5603AQHf7VJG-0wVug/profile-displayphoto-shrink_800_800/0/1644000811499?e=1718236800&v=beta&t=Q5foIfAxuxeIMLbyGskDzoV1i6Ic3BMGBJeIwgZkNwQ",
+          name: "Harish Kumar",
+          company: "Paras Technologies",
+          designation: "Founder & CEO",
+          profile: "",
+          twitter: "https://twitter.com/hifiveyatin",
+          linkedin: "https://www.linkedin.com/in/hifiveyatin/",
+          bio: "Yatin is a Project Engineer by profession having 3+ years of experience in corporate field. Learning new technology and implementing them is his goal. Along with his daily life, he also try to give back to the society by sharing information to peers."
+        },
+        {
+          id: 3,
+          image: "https://media.licdn.com/dms/image/D5603AQGV5uzbY5cT_Q/profile-displayphoto-shrink_800_800/0/1686818441933?e=1718236800&v=beta&t=1BCENIx2Jf08IuwIqE8D_a2dJs3wBulvvO-N1UvyGLg",
+          name: "Arun Kumar",
+          company: "Tech Exponent System",
+          designation: "Director",
+          profile: "",
+          twitter: "https://twitter.com/loveleen_nancy",
+          linkedin: "https://www.linkedin.com/in/loveleen-kaur/",
 
-              bio: "Loveleen Kaur, a bundle of positivity and happiness, this tech-savvy girl loves mountains and works wonders with technology. She is currently working as an Senior Android Developer with 5 years of experience. She is Co-Organizer of GDG Chandigarh, WTM Ambassador and Google Android Educator. Hailing from the technology field, she loves logic, but also has a passion for creativity. In her free time, She loves to mentor people to help them learn and grow in their career."
-          },
+          bio: "Loveleen Kaur, a bundle of positivity and happiness, this tech-savvy girl loves mountains and works wonders with technology. She is currently working as an Senior Android Developer with 5 years of experience. She is Co-Organizer of GDG Chandigarh, WTM Ambassador and Google Android Educator. Hailing from the technology field, she loves logic, but also has a passion for creativity. In her free time, She loves to mentor people to help them learn and grow in their career."
+        },
 
       ],
       eventType: [
-          "Ideathon", "Tech Talk", "AI",
-          "Android",
-          "Cloud",
-          "Machine Learning",
-          "Mobile",
-          "Web",
-          "Women Techmakers"
+        "Ideathon", "Tech Talk", "AI",
+        "Android",
+        "Cloud",
+        "Machine Learning",
+        "Mobile",
+        "Web",
+        "Women Techmakers"
       ],
       guest_type: "Judge",
       partners: [
         "https://www.drupal.org/files/infosys-logo-PNG.png",
         "https://media.licdn.com/dms/image/D4D0BAQFJ_9CR1sASHw/company-logo_200_200/0/1687872437902/seed_global_education_logo?e=2147483647&v=beta&t=B4A_7P3Iow5sR8aSbnm-l_XC-px6Q6IwmlKjvAQvbUU",
         "https://www.finlatics.com/landing/img/brand.png",
-        ]
-  },
+      ]
+    },
     {
-      id:0,
+      id: 0,
       chapter: "The Uniques | SVIET",
       headerImage: "https://res.cloudinary.com/startup-grind/image/upload/c_limit,dpr_2,f_auto,g_center,h_1440,q_auto:good,w_2048/v1/gcs/platform-data-goog/event_wrapup/DSC05771.JPG",
       logo: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/Bevy%20Thumbnail_3yFLs9d.png",
@@ -330,7 +330,7 @@ function App() {
       ]
     },
     {
-      id:1,
+      id: 1,
       chapter: "The Uniques | SVIET",
       headerImage: "https://media.licdn.com/dms/image/D4D22AQEL6ULQQjmG6A/feedshare-shrink_2048_1536/0/1698642601892?e=1715212800&v=beta&t=_2O83ldmUbezl9BKWB342FaqhWalhWZPeTA_9BBRyN0",
       logo: "https://assets.devfolio.co/hackathons/d111a298717f42e0b647027976e265b0/assets/cover/87.jpeg",
@@ -415,7 +415,7 @@ function App() {
       ]
     },
     {
-      id:2,
+      id: 2,
       chapter: "The Uniques | SVIET",
       headerImage: "https://pbs.twimg.com/media/FqSl9vhaAAE0Vlu?format=jpg&name=4096x4096",
       logo: sm,
@@ -448,12 +448,12 @@ function App() {
       ],
       guest_type: "Chief Guest",
       partners: [
-        "https://sviet.ac.in/wp-content/uploads/2021/08/svietlogooo-1-400x83.png",
+       "https://utfs.io/f/48f81269-d902-40c8-a9c0-1d711c20611b-m3rpck.webp",
         "http://chaithela.com/images/logo.png"
       ]
     },
     {
-      id:3,
+      id: 3,
       chapter: "The Uniques | SVIET",
       headerImage: "https://pranav-s-portfolio.web.app/assets/events/Workshops/SVIET/img1.jpg",
       logo: "https://www.gstatic.com/images/branding/product/2x/firebase_64dp.png",
@@ -486,10 +486,51 @@ function App() {
       ],
       guest_type: "Chief Guest",
       partners: [
-        "https://sviet.ac.in/wp-content/uploads/2021/08/svietlogooo-1-400x83.png",
+        "https://utfs.io/f/48f81269-d902-40c8-a9c0-1d711c20611b-m3rpck.webp",
         "https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-goog/contentbuilder/logo_dark_QmPdj9K.svg"
       ]
     },
+    {
+      id: 6,
+      chapter: "The Uniques | SVIET",
+      headerImage: "https://utfs.io/f/33b5ab2e-eb5b-4448-a249-d823fc3d6b7f-ch8zt2.jpg",
+      logo: "https://utfs.io/f/d6e72f0f-e5ab-47e0-86ae-2ac86264edd1-lbyfkk.avif",
+      name: "Drive into the world of blockchain",
+      date: "1st April 2023",
+      venue: "SVIET Auditorium",
+      overview: "An expert talk on blockchain technology by Udayveer Singh. Join Udayveer and industry leaders as they explore the latest advancements in blockchain, including decentralized finance (DeFi), NFTs, and smart contracts. Gain deep insights into how blockchain is reshaping industries and learn from the forefront of innovation in this rapidly evolving field. ",
+      speakers: [
+        {
+          id: 2,
+          image: "https://utfs.io/f/b1c7ae51-6297-4347-bb3c-f0c4a76efda7-hqsv6i.jpeg",
+          name: "Udayveer singh",
+          company: "Antire solutions",
+          designation: "Blockchain Engineerr",
+          profile: "",
+          twitter: "https://twitter.com/hifiveyatin",
+          linkedin: "https://www.linkedin.com/in/hifiveyatin/",
+          bio: "Yatin is a Project Engineer by profession having 3+ years of experience in corporate field. Learning new technology and implementing them is his goal. Along with his daily life, he also try to give back to the society by sharing information to peers."
+        },
+
+      ],
+      eventType: [
+        "Blockchain",
+        "Cryptocurrency",
+        "Smart Contracts",
+        "Decentralized Finance (DeFi)",
+        "Bitcoin",
+        "Ethereum",
+
+        "Tokenomics",
+        "Blockchain Security",
+      ],
+      guest_type: "Chief Guest",
+      partners: [
+        "https://utfs.io/f/48f81269-d902-40c8-a9c0-1d711c20611b-m3rpck.webp",
+        "https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-goog/contentbuilder/logo_dark_QmPdj9K.svg"
+      ]
+    },
+
   ]
 
   const Layout = () => {
@@ -497,7 +538,7 @@ function App() {
       <div>
         <Navbar />
         <div className='pt-20'>
-        <Outlet />
+          <Outlet />
         </div>
         <Foot />
       </div>
@@ -512,16 +553,16 @@ function App() {
       children: [
         {
           path: '/',
-          element: <About blogs={blogData}  handleClick={handleClick}/>
+          element: <About blogs={blogData} handleClick={handleClick} />
         },
         {
           path: '/clubs',
-          element: <Clubs event={event}  handleClick={handleClick}/>
+          element: <Clubs event={event} handleClick={handleClick} />
 
         },
         {
           path: '/communityguidelines',
-          element: <CommGuide  handleClick={handleClick}/>
+          element: <CommGuide handleClick={handleClick} />
         },
         {
           path: '/leads',
@@ -537,31 +578,31 @@ function App() {
         },
         {
           path: '/campus-ambassador/create-event',
-          element: <EventForm  handleClick={handleClick}/>
+          element: <EventForm handleClick={handleClick} />
         },
         {
           path: '/campus-ambassador/event-list',
-          element: <EventList  handleClick={handleClick}/>
+          element: <EventList handleClick={handleClick} />
         },
         {
           path: '/campus-ambassador/event/:id',
-          element: <Event events={event}  handleClick={handleClick}/>
+          element: <Event events={event} handleClick={handleClick} />
         },
         {
           path: '/chapter',
-          element: <Campus event={event}  handleClick={handleClick}/>
+          element: <Campus event={event} handleClick={handleClick} />
         },
         {
           path: '/blogs',
-          element: <Blogs  blogs={blogData} handleClick={handleClick}/>
+          element: <Blogs blogs={blogData} handleClick={handleClick} />
         },
         {
           path: '/blogs/:blogTitle',
-          element: <BlopPage  blogs={blogData} handleClick={handleClick}/>
+          element: <BlopPage blogs={blogData} handleClick={handleClick} />
         },
         {
           path: '/lead-application',
-          element: <Lead  blogs={blogData} handleClick={handleClick}/>
+          element: <Lead blogs={blogData} handleClick={handleClick} />
         },
       ]
     }
