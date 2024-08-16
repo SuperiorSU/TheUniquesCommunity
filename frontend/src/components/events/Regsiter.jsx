e8da2d8dbdb4094f8b36f3f244a8ff65ca054336
SuperiorSU <sujalsinha2001@gmail.com>
2024-08-16 12:16:29 +0530

    Register Done

--- frontend/src/components/events/Regsiter.jsx
+++ frontend/src/components/events/Regsiter.jsx
@@ -1,7 +1,7 @@
 import { CalendarDaysIcon, MapIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-const Regsiter = ({date,venue,formHandler}) => {
+const Regsiter = ({date,venue,formHandler=null}) => {
   
   return (
     <div className='w-10/12 mx-auto bg-white p-4 rounded-md shadow-md flex flex-wrap-reverse justify-between items-center'>
@@ -10,7 +10,7 @@ const Regsiter = ({date,venue,formHandler}) => {
         <p className="text-lg font-medium flex items-center gap-3 flex-wrap"><MapIcon width={20} height={20} className='flex-shrink-0' />Venue : {venue}</p>
 
       </div>
-      <button onClick={formHandler} className='bg-red-800 text-white px-2 py-1 rounded-md mb-6 lg:mb-0 w-full lg:w-fit lg:h-auto'>Register</button>
+      {formHandler&& <button onClick={formHandler} className='bg-red-800 text-white px-2 py-1 rounded-md mb-6 lg:mb-0 w-full lg:w-fit lg:h-auto'>Register</button>}
     </div>
   )
 }
